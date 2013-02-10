80862a1af53281e587f25a1becb015627de38229
Sir.Thorgeir <thorgeirk11@ru.is>
2013-02-10 21:50:09 +0000

    BreathSearch is ready for testing

--- vacuumcleaner_utility_agent/src/State.java
+++ vacuumcleaner_utility_agent/src/State.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class State
 {
-	public State(Point curPos, Point prevPos, ArrayList<Point> bumps,ArrayList<Point> dirt)
+	public State(Point curPos, State prevPos, ArrayList<Point> bumps,ArrayList<Point> dirt)
 	{
 		this.curPos = curPos;
 		this.prevPos = prevPos;
@@ -13,19 +13,19 @@ public class State
 	public ArrayList<Point> bumps;
 	public ArrayList<Point> dirt;
 	
-	public Point prevPos;
+	public State prevPos;
 	public Point curPos;
 	
 	ArrayList<State> legalMoves(int height, int width) {
 		ArrayList<State> l = new ArrayList<State>();
 		if (curPos.x + 1 < height)
-			l.add(new State(new Point(curPos.x + 1,curPos.y),curPos,bumps,dirt));
+			l.add(new State(new Point(curPos.x + 1,curPos.y),this,bumps,dirt));
 		if (curPos.x - 1 >= 0)                                  
-			l.add(new State(new Point(curPos.x - 1,curPos.y),curPos,bumps,dirt));
+			l.add(new State(new Point(curPos.x - 1,curPos.y),this,bumps,dirt));
 		if (curPos.y + 1 < width)     
-			l.add(new State(new Point(curPos.x,curPos.y + 1),curPos,bumps,dirt));
+			l.add(new State(new Point(curPos.x,curPos.y + 1),this,bumps,dirt));
 		if (curPos.y - 1 >= 0)        
-			l.add(new State(new Point(curPos.x,curPos.y - 1),curPos,bumps,dirt));
+			l.add(new State(new Point(curPos.x,curPos.y - 1),this,bumps,dirt));
 		
 		for (Point p : bumps)
 			for (int i = 0; i < l.size(); i++)
@@ -43,7 +43,7 @@ public class State
 	}
 	
 	State DoMove(int x, int y) {
-		State s = new State(new Point(x,y),curPos,bumps,dirt);
+		State s = new State(new Point(x,y),this,bumps,dirt);
 		for (Point d : dirt)
 			if (s.curPos.equals(d))
 				s.dirt.remove(s);
