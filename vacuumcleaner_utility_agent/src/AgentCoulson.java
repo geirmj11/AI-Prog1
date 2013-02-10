c427246117301e9dfbbaf26bbe50b5042667c840
geirmj11 <geirmj11@ru.is>
2013-02-10 22:55:03 +0000

    agent commentlined

--- vacuumcleaner_utility_agent/src/AgentCoulson.java
+++ vacuumcleaner_utility_agent/src/AgentCoulson.java
@@ -70,7 +70,7 @@ public class AgentCoulson implements Agent
 				System.err.println("strange percept that does not match pattern: " + percept);
 			}
 		}
-	    path = breadthsearch.getPath(this.home, this.size, this.dirt, this.obstacles);
+		this.path = BreathSearch.getPath(this.home, this.size, this.dirt, this.obstacles);
     }
 
     public String nextAction(Collection<String> percepts) {
@@ -80,55 +80,86 @@ public class AgentCoulson implements Agent
         //String[] actions = { "TURN_ON", "TURN_OFF", "TURN_RIGHT", "TURN_LEFT", "GO", "SUCK" };
         if(!power) {
             power = true;
+            System.out.println("------------Power on:");
             return "TURN_ON";
         }
+        if(path == null)             System.out.println("------------NULL!!:");
 	    if(path.empty()) {
 	        power = false;
+            System.out.println("------------- Power off:");
     	    return "TURN_OFF";
     	}
     
-		for (String p : percepts) {
-			if (p.equals("DIRT"))
-				return "SUCK";
-		}
+		//for (String p : percepts) {
+		//	if (p.equals("DIRT"))
+		//		return "SUCK";
+		//}
    
 	    Point dest = path.peek();
         if(dest.x == this.x){
 	        if(dest.y > this.y){
 	            if(orientation == 0){
                     path.pop();
+                                System.out.println("------------GO:");
 	                return "GO";
 	            }
-	            if(orientation == 1) return "TURN_LEFT";
-	            else return "TURN_RIGHT";
+	            if(orientation == 1) {
+	                System.out.println("------------TURN Left");     
+	                return "TURN_LEFT";
+	            }
+	            else {
+                    System.out.println("------------Turn right:");
+	                return "TURN_RIGHT";
+	            }
 	        }
 	        if(dest.y < this.y){
 	            if(orientation == 2){
 	                path.pop();
 	                return "GO";
 	            }
-	            if(orientation == 3) return "TURN_LEFT";
-	            else return "TURN_RIGHT";
+	            if(orientation == 3) {
+                    System.out.println("------------Left:");
+	                return "TURN_LEFT";
+                }
+	            else {
+                    System.out.println("------------Right:");
+                    return "TURN_RIGHT";
+                }
 	        }
 	    }
 	    else{
 	        if(dest.x > this.x){
 	            if(orientation == 1){
                     path.pop();
+                    System.out.println("------------gow:");
 	                return "GO";
 	            }
-	            if(orientation == 2) return "TURN_LEFT";
-	            else return "TURN_RIGHT";
+	            if(orientation == 2){
+                    System.out.println("------------lewt:");
+	                return "TURN_LEFT";
+	            }
+	            else {
+	                System.out.println("------------riwt:");   
+	                return "TURN_RIGHT";
+	            }
 	        }
 	        if(dest.x < this.x){
 	            if(orientation == 3){
 	                path.pop();
+                    System.out.println("------------goW:");
 	                return "GO";
 	            }
-	            if(orientation == 0) return "TURN_LEFT";
-	            else return "TURN_RIGHT";
+	            if(orientation == 0) {
+                    System.out.println("------------lewttt:");
+	                return "TURN_LEFT";
+	            }
+	            else {
+                    System.out.println("------------rojt:");   
+	                return "TURN_RIGHT";
+	            }
 	        }
 	    }
+        System.out.println("------------Power OFF! D:");
         return "TURN_OFF";   
 	}    
 }
